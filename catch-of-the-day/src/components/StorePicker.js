9815154412ba32067061d2619ce5e9939d57d339
Stacey Stewart <stacey.alicia.stewart@gmail.com>
2017-07-10 16:20:10 -0400

    sets up the app

---
+++ catch-of-the-day/src/components/StorePicker.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class StorePicker extends React.Component {
+  render() {
+    return (
+      <form className="store-selector">
+
+      </form>
+    )
+  }
+};
+
+export default StorePicker;
