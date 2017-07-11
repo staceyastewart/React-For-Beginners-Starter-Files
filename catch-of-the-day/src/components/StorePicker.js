3d5a79721751a2d9ef33cca353beda46b9e05263
Stacey Stewart <stacey.alicia.stewart@gmail.com>
2017-07-11 15:19:57 -0400

    adds css

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -4,7 +4,10 @@ class StorePicker extends React.Component {
   render() {
     return (
       <form className="store-selector">
-
+        {/* this is a comment that must be inside the element */}
+        <h2>Please Enter a Store</h2>
+        <input type="text" required placeholder="Store Name" />
+        <button type="submit">Visit Store</button>
       </form>
     )
   }
